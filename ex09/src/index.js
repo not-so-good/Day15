b1e80536e1d343f24337ce30016dea56a20fc152
not-so-good <sanjin.suljanovic@gmail.com>
2021-03-22 20:25:43 +0100

    ex09 The rest parameter with function parameters

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -1,7 +1,7 @@
 //Only change code below this line
 const product = (...args) =>{
   //Only change code above this line(
-  return args.reduce((a, b) => a * b, 1); //Promijenio i ovu liniju, umetnuo 1 na kraju
+  return args.reduce((a, b) => a * b, 1); 
 }
 console.log(product(1, 2)); //Change this line
 console.log(product(4, 3, 10, 2));
